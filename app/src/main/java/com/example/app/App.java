ee08ffb383ce63511ea5c6f516662e66734f4c76
Minokainduwara <minokainduwara@gmail.com>
2025-04-07 12:20:36 +0530

    stage

--- app/src/main/java/com/example/app/App.java
+++ app/src/main/java/com/example/app/App.java
@@ -1,7 +1,11 @@
 package com.example.app;
 
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
 public class App {
     public static void main(String[] args) {
-
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Stage stage = new Stage();
     }
 }
