4427da748c89c71422026c2b57f8ad6b7c1c2622
Minokainduwara <minokainduwara@gmail.com>
2025-04-12 14:23:19 +0530

    login button created

--- app/src/main/java/com/example/app/Controllers/LoginController.java
+++ app/src/main/java/com/example/app/Controllers/LoginController.java
@@ -4,6 +4,8 @@ import javafx.scene.control.Button;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 
@@ -18,8 +20,20 @@ public class LoginController {
     @FXML
     private Label loginErrormsg;
 
+    @FXML
+    private TextField userNameField;
+
+    @FXML
+    private PasswordField PasswordField;
+
     public void LoginBtnOnAction(javafx.event.ActionEvent actionEvent) {
-        loginErrormsg.setText("You try to login");
+
+        //Validation
+        if (userNameField.getText().isBlank() == false && PasswordField.getText().isBlank() == false) {
+            loginErrormsg.setText("You try to login again");
+        } else {
+            loginErrormsg.setText("Please enter your username and password");
+        }
     }
 
     public void CancelBtnOnAction(javafx.event.ActionEvent actionEvent) {
