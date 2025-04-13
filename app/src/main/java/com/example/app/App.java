f9fd52c248971936c629d20abbaeb52d86708f99
Dilhara-hub <dilharasamaranayake74@gmail.com>
2025-04-13 10:26:44 +0530

    add image student.fxml

--- app/src/main/java/com/example/app/App.java
+++ app/src/main/java/com/example/app/App.java
@@ -1,11 +1,16 @@
 package com.example.app;
-
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 
-public class App {
-    public static void main(String[] args) {
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        Stage stage = new Stage();
+public class App extends Application{
+    double x,y;
+    @Override
+    public void start(Stage primarystage) throws Exception {
+       Parent root = FXMLLoader.load(getClass().getResource("Views/Login.fxml"));
+       primarystagestage.setTitle("Student Dashboard");
     }
 }
+    public static void main(String[] args) {
+      launch(args);
+    }
