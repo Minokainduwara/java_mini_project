6b205ec1ae9307a24c5cf4d58181c650c91baa42
Minokainduwara <minokainduwara@gmail.com>
2025-04-17 12:44:41 +0530

    error fixed at last I am so happy

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -14,7 +14,7 @@ module com.example.app {
     requires java.sql;
 
     opens com.example.app.Controllers to javafx.fxml;
-
+    opens com.example.app.Controllers.Admin to javafx.fxml;
     opens com.example.app to javafx.fxml;
     exports com.example.app;
     exports com.example.app.Controllers.Admin to javafx.fxml;
