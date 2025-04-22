149bc6d634cff7d5a027df88de76759dedeb94b6
Minokainduwara <minokainduwara@gmail.com>
2025-04-22 11:40:11 +0530

    dashboards created

--- app/src/main/java/com/example/app/Controllers/Admin/AdminController.java
+++ app/src/main/java/com/example/app/Controllers/Admin/AdminController.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 
@@ -19,12 +20,31 @@ public class AdminController {
     private Button logoutBtn;
 
     @FXML
+    private Button dashboardbtn;
+
+    @FXML
+    private Button userBtn;
+
+    @FXML
+    private Button courseBtn;
+
+    @FXML
+    private Button timetableBtn;
+
+    @FXML
+    private AnchorPane paneCenter;
+
+    @FXML
     private ImageView profileImg;
 
     @FXML
     public void initialize() {
         // Optional: If you don't want to modify FXML
         logoutBtn.setOnAction(this::logout);
+        dashboardbtn.setOnAction(this::handleDashboard);
+        userBtn.setOnAction(this::handleUser);
+        courseBtn.setOnAction(this::handleCourse);
+        timetableBtn.setOnAction(this::handleTimetable);
     }
 
     public void logout(ActionEvent event) {
@@ -41,4 +61,39 @@ public class AdminController {
             e.printStackTrace();
         }
     }
+
+    private void loadUI(String fileName) {
+        try {
+            AnchorPane pane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Fxml/Admin/" + fileName)));
+            paneCenter.getChildren().setAll(pane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+    private void handleDashboard(ActionEvent event) {
+        loadUI("dashboard.fxml");
+    }
+
+
+    private void handleUser(ActionEvent event) {
+        loadUI("User.fxml");
+    }
+
+
+    private void handleCourse(ActionEvent event) {
+        loadUI("Courses.fxml");
+    }
+
+
+    private void handleTimetable(ActionEvent event) {
+        loadUI("Timetable.fxml");
+    }
+
+
+
 }
