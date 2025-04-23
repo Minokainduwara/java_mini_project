378342243b04588d26aeb03ac961c6625e875a13
Dilhara-hub <dilharasamaranayake74@gmail.com>
2025-04-23 21:27:55 +0530

    add fxml and related controllers

---
+++ app/src/main/java/com/example/app/Controllers/Student/AttendenceController.java
@@ -0,0 +1,12 @@
+package com.example.app.Controllers.Lecturer;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
