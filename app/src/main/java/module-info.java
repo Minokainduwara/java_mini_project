ec6c9ce0384062f5b4e56155ae365df6dcc2fe40
MalinduShasan <malindushasan@gmail.com>
2025-04-27 12:33:27 +0530

    Added Lecturer to module-info.java

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -13,6 +13,11 @@ module com.example.app {
     requires java.desktop;
     requires java.sql;
 
+    opens com.example.app.Controllers to javafx.fxml;
+    opens com.example.app.Controllers.Admin to javafx.fxml;
+    opens com.example.app.Controllers.Lecturer to javafx.fxml;
     opens com.example.app to javafx.fxml;
     exports com.example.app;
+    exports com.example.app.Controllers.Admin to javafx.fxml;
+    exports com.example.app.Controllers.Lecturer to javafx.fxml;
 }
